3f3fe4bcdc73845045d6f0ac1528fdb7370de62a
Aarush-Parashar <aarushparashar2004@gmail.com>
2024-05-30 19:33:55 +0530

    added navigation from productlist to product deatails

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_app_flutter/global_variables.dart';
-import 'package:shopping_app_flutter/product_card.dart';
+import 'package:shopping_app_flutter/product_list.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,109 +9,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final List<String> filters = const ['All', 'Addidas', 'Nike', 'Bata'];
-  late String selectedFilter;
+  int currentPage = 0;
 
   @override
-  void initState() {
-    super.initState();
-    selectedFilter = filters[0];
-  }
-
   @override
   Widget build(BuildContext context) {
-    const border = OutlineInputBorder(
-      borderSide: BorderSide(
-        color: Color.fromRGBO(225, 225, 225, 1),
-      ),
-      borderRadius: BorderRadius.horizontal(
-        left: Radius.circular(50),
+    return Scaffold(
+      body: const ProductList(),
+      bottomNavigationBar: SizedBox(
+        height: 80,
+        child: BottomNavigationBar(
+          onTap: (value) {
+            setState(() {
+              currentPage = value;
+            });
+          },
+          currentIndex: currentPage,
+          items: const [
+            BottomNavigationBarItem(
+              label: '',
+              icon: Icon(
+                Icons.home,
+                size: 45,
+              ),
+            ),
+            BottomNavigationBarItem(
+              label: '',
+              icon: Icon(
+                Icons.shopping_bag,
+                size: 45,
+              ),
+            )
+          ],
+        ),
       ),
     );
-
-    return Scaffold(
-        body: SafeArea(
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text('Shoes \nCollection',
-                    style: Theme.of(context).textTheme.titleLarge),
-              ),
-              const Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Search",
-                    prefixIcon: Icon(
-                      Icons.search,
-                    ),
-                    border: border,
-                    enabledBorder: border,
-                    focusedBorder: border,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 80,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: filters.length,
-              itemBuilder: (context, index) {
-                final filter = filters[index];
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8),
-                  child: GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        selectedFilter = filter;
-                      });
-                    },
-                    child: Chip(
-                      label: Text(filter),
-                      labelStyle: const TextStyle(
-                        fontSize: 16,
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 20,
-                        vertical: 15,
-                      ),
-                      backgroundColor: selectedFilter == filter
-                          ? Theme.of(context).colorScheme.primary
-                          : const Color.fromRGBO(245, 247, 249, 1),
-                      side: const BorderSide(
-                        color: Color.fromRGBO(245, 247, 249, 1),
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: products.length,
-              itemBuilder: (context, index) {
-                final product = products[index];
-                return ProductCard(
-                  title: product['title'] as String,
-                  price: product['price'] as double,
-                  image: product['imageUrl'] as String,
-                  backgroundColor: index.isEven
-                      ? const Color.fromRGBO(216, 240, 253, 1)
-                      : const Color.fromRGBO(245, 247, 249, 1),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    ));
   }
 }
--- lib/product_details.dart
+++ lib/product_details.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_app_flutter/global_variables.dart';
 
 class ProductDetailPage extends StatefulWidget {
   final Map<String, Object> product;
